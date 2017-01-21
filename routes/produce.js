82a9fd6c6789f8141d3a447988c7afe3aac1e7ff
Brad Chapin <paledoor@yahoo.com>
2017-01-21 13:06:52 -0700

    write produce routes

--- routes/produce.js
+++ routes/produce.js
@@ -4,4 +4,85 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
+router.get("/", (req, res, next) => {
+  knex("produce")
+  .then (results => {
+    if (results.length === 0) {
+      return res.send (404);
+    }
+    return res.status(200).send(results);
+  });
+  .catch(err => {
+    next(err);
+  })
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  knex ("produce")
+  .where("produce.id", id)
+  .first()
+  .then(result => {
+    if (!result) {
+      return res.send(404);
+    }
+    return res.send(200).send(result);
+  })
+  .catch (err => {
+    next(err);
+  });
+});
+
+router.post("/", (req, res, next) => {
+  const name = req.params.name;
+  const newProduce = name;
+  knex ("produce")
+  insert(newProduce)
+  .then(result => {
+    return res.status(200).send(newProduce);
+  })
+  .catch(err =>{
+    next(err);
+  });
+});
+
+router.patch("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const name = req.params.name;
+  const updatedProduce = name;
+
+  knex("produce")
+  .where("produce.id", id)
+  .update(updatedProduce, "*")
+  .then(success => {
+    knex("produce")
+    .where("produce.id")
+    .first()
+    .then(result => {
+      return res.status(200).send(result);
+    })
+    .catch (err => {
+      next(err);
+    });
+  })
+  .catch(err => {
+    next(err);
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+
+  knex("produce")
+  .where("produce.id", id)
+  .del()
+  .then(result => {
+    return res.send(200);
+  })
+  .catch (err => {
+    next(err);
+  });
+});
+
 module.exports = router;
