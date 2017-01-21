818e18c72c6911d0046703e200c28d16ccd297bd
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-21 13:52:16 -0700

    Server update

--- server.js
+++ server.js
@@ -36,33 +36,28 @@ const users = require('./routes/users');
 app.use('/authenticate', authenticate);
 
 app.use(function (req, res, next) {
-
   // check header or url parameters or post parameters for token
   var token = req.body.token || req.query.token || req.headers['Authorization'];
 
   // decode token
   if (token) {
-
     // verifies secret and checks exp
     jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
       if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });
+        return res.json({ success: false, message: 'Failed to confirm the token.' });
       } else {
         // if everything is good, save to request for use in other routes
         req.decoded = decoded;
         next();
       }
     });
-
   } else {
-
     // if there is no token
     // return an error
-    return res.status(403).send({
+    return res.status(401).send({
       success: false,
-      message: 'No token provided.'
+      message: 'Unauthorized.'
     });
-
   }
 });
 
