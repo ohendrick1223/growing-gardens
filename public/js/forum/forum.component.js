daf77a8dc80866c0c46bd3c9685cdea1078001a9
Olivia <ohendrick1223@gmail.com>
2017-01-23 13:43:12 -0700

    got get all posts displaying on page

--- public/js/forum/forum.component.js
+++ public/js/forum/forum.component.js
@@ -25,7 +25,7 @@
 
     function getPosts() {
       let displayedPosts = [];
-      $http.get('/posts').then(function(result) {
+      $http.get('api/posts').then(function(result) {
         vm.category = $stateParams.category;
         displayedPosts = [];
         //TODO refactor with a filter HOF
@@ -35,8 +35,8 @@
             displayedPosts.push(result.data[i]);
           } else if (result.data[i].category === vm.category) {
             displayedPosts.push(result.data[i]);
-          }
         }
+      } console.log(displayedPosts);
         // Organize data by want/need
         populateColumns(displayedPosts);
       });
@@ -51,7 +51,7 @@
         } else {
           vm.havePosts.push(posts[i]);
         }
-      }
+      } console.log("NEED ARRAY: ", vm.needPosts, "HAVE ARRAY: ", vm.havePosts);
     }
 
     vm.uploadPhoto = function() {
@@ -74,6 +74,21 @@
       ModalService.showModal({
         templateUrl: "js/forum/modal.html",
         controller: function($scope, $element, close) {
+          // $scope.uploadPhoto = function() {
+          //   console.log("button firing!!");
+          //   cloudinary.openUploadWidget({
+          //       cloud_name: 'ohendrick1223',
+          //       upload_preset: 'zpfcnfn1'
+          //     },
+          //     function(error, result) {
+          //       if (error) {
+          //         console.error(error);
+          //       }
+          //       var photoURL = result[0].secure_url;
+          //       // Display photo preview
+          //       console.log(photoURL);
+          //     });
+          // };
           $scope.myClose = function(result) {
             $element.modal('hide');
             close(null, 500);
