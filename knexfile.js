2d756f79e926461efbd238f2c2848ee84c7bde0c
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-23 10:16:32 -0700

    update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -14,12 +14,13 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    connection: {
-      host: 'us-cdbr-iron-east-02.cleardb.net',
-      user: 'b13910f57ab565',
-      password: 'd0f6cd3e',
-      database: 'heroku_b53d242096e38d5',
-    }
+    // connection: {
+    //   host: 'us-cdbr-iron-east-02.cleardb.net',
+    //   user: 'b13910f57ab565',
+    //   password: 'd0f6cd3e',
+    //   database: 'heroku_b53d242096e38d5',
+    // }
+    connection: 'mysql://b13910f57ab565:d0f6cd3e@us-cdbr-iron-east-04.cleardb.net/heroku_b53d242096e38d5?reconnect=true'
   }
 };
 
