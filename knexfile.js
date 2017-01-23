8522c4f0aa21fed3bb160b7729db29f6da3c843e
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-23 10:13:56 -0700

    Update knexfile.js for deployment

--- knexfile.js
+++ knexfile.js
@@ -14,15 +14,13 @@ module.exports = {
 
   production: {
     client: 'mysql',
-    // connection: {
-    //   host: process.env.DB_HOST,
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_NAME,
-    // }
-    connection: process.env.CLEARDB_DATABASE_URL
+    connection: {
+      host: 'us-cdbr-iron-east-02.cleardb.net',
+      user: 'b13910f57ab565',
+      password: 'd0f6cd3e',
+      database: 'heroku_b53d242096e38d5',
+    }
   }
-
 };
 
 
