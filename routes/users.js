28a6537d7b70d16477b29db97619fbb83e63fc47
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-23 17:34:23 -0700

    Fix user route

--- routes/users.js
+++ routes/users.js
@@ -28,31 +28,6 @@ router.get('/:id', (req, res, next) => {
   }
 });
 
-router.post('/', (req, res, next) => {
-  const { first_name, last_name, email, password } = req.body;
-  bcrypt.hash(password, 10).then(hashed_password => {
-      const newUser = { first_name, last_name, email, hashed_password };
-      return knex('users')
-        .insert(newUser, '*')
-        .then(success => {
-          return knex('users')
-            .where('users.id', success[0])
-            .then(result => {
-              return res.status(200).send(result);
-            })
-            .catch(err => {
-              next(err);
-            });
-        })
-        .catch(err => {
-          next(err);
-        });
-    })
-    .catch(err => {
-      next(err);
-    });
-});
-
 router.patch('/:id', (req, res, next) => {
   const id = req.params.id;
   const { first_name, last_name, email, password } = req.body;
