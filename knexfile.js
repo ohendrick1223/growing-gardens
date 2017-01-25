733c95a2058a785b6cfd85ebf7a42aa3ed4abc80
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-25 00:19:01 -0700

    attempt deployment

--- knexfile.js
+++ knexfile.js
@@ -16,11 +16,15 @@ module.exports = {
     client: 'mysql',
     connection: 'mysql://b13910f57ab565:d0f6cd3e@us-cdbr-iron-east-04.cleardb.net/heroku_b53d242096e38d5?reconnect=true',
     migrate: 'safe',
-    connectionLimit: 13,
-    waitForConnections: true,
     pool: {
-      min: 1,
-      max: 14
+      min: 0,
+      max: 7
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
   }
 };
