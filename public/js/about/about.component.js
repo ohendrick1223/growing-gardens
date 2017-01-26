49fdd5c6fb6c8693029e280557acd3ed9c2d6ec1
oliviaridgley <oliviatateridgley@gmail.com>
2017-01-25 17:49:14 -0700

    Added frameworking and initial styling to about page

--- public/js/about/about.component.js
+++ public/js/about/about.component.js
@@ -11,32 +11,28 @@
   function controller($http, $state, $stateParams) {
     const vm = this;
 
-
     vm.slides = [];
     vm.myInterval = 3000;
     vm.noWrapSlides = false;
     vm.active = 0;
+
     vm.$onInit = function() {
-      console.log("home initialized");
+
       vm.slides = [{
           image: '../../assets/carousel/slider1.jpg',
           id: 0,
-          // text: 'The Greenhouse'
         },
         {
           image: '../../assets/carousel/slider2.jpg',
           id: 1,
-          // text: 'Greetings From the Internet'
         },
         {
           image: '../../assets/carousel/slider3.jpg',
           id: 2,
-          // text: 'Stone Soup'
         },
         {
           image: '../../assets/carousel/slider4.jpg',
           id: 3,
-          // text: 'Fridge Face'
         }
       ];
 
