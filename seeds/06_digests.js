61afb9a8f5fab201073c94de159c407a9a9370d7
Matthew Lindly <matthew.lindly@hotmail.com>
2017-01-27 10:54:04 -0700

    Update seeds last time

--- seeds/06_digests.js
+++ seeds/06_digests.js
@@ -7,17 +7,22 @@ exports.seed = function (knex, Promise) {
         knex("digests").insert({
           id: 1,
           user_id: 1,
-          message: "Does anyone wanna join me for gardening this beautiful, cold, afternoon?"
+          message: "I think I forgot my trowel in the shed at Centennial, can anyone check for me if they're stopping by that garden today?"
         }),
         knex("digests").insert({
           id: 2,
           user_id: 3,
-          message: "I really need a shovel in the Centennial Garden, can someone bring me one? You're a life-saver!"
+          message: "I am at Centennial right and it's pouring, so watch out if you're headed this way!"
         }),
         knex("digests").insert({
           id: 3,
           user_id: 2,
-          message: "On my way. Shovel in hand."
+          message: "Pretty muddy in Hawthorn as well."
+        }),
+        knex("digests").insert({
+          id: 4,
+          user_id: 5,
+          message: "Hey Matt, your trowel is still here! I put it in the back left corner to make sure it's still here when you come back for it."
         })
       ]);
     })
