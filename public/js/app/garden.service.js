d4e86ca40c572c262fd6e6027ec0652c91e3504e
Anna Lotko <annaklotko@gmail.com>
2017-02-03 09:31:20 -0700

    cleaned up garden service

--- public/js/app/garden.service.js
+++ public/js/app/garden.service.js
@@ -5,30 +5,26 @@
     .service('gardenService', function() {
       this.gardenID = "gardenID";
       this.gardenPlots = [];
-      var that = this;
-      //TODO call this on init in each garden
-      this.getPlots = function($http, thisFarm) {
 
+      this.getPlots = function($http, thisGarden) {
+        // Get all plot data and filter by garden
         $http.get('/api/plots').then(function(result) {
           var plots = [];
           for (let i = 0; i < result.data.length; i++) {
-            if (result.data[i].farm === thisFarm) {
+            if (result.data[i].farm === thisGarden) {
               plots.push(result.data[i]);
             }
           }
-          let itr = 0;
-          // console.log(plots);
-          // Get all SVG clusters for each plot and add cluster to object reference
-          for (let i = plots[0].id; i <= plots[plots.length-1].id; i++) {
-            // svgClusters start at 1 becuase of the naming in SVG files, hence i+1
-            plots[itr].svgCluster = angular.element('#p'+i).children();
 
-            // console.log(itr, plots[itr].svgCluster);
+          // Get all SVG clusters for each plot and add cluster to object reference
+          let itr = 0;
+          for (let i = plots[0].id; i <= plots[plots.length-1].id; i++) {
+            plots[itr].svgCluster = angular.element('#p'+i).children();
             itr++;
           }
-          console.log(result);
-          var promiseArr = [];
+
           // Add an array of produce data to the plot and populate it
+          var promiseArr = [];
           for (let i = 0; i < plots.length; i++) {
             // i needs to start at 209
             let plotId = plots[0].id + i;
@@ -39,10 +35,8 @@
 
           Promise.all(promiseArr).then(function(results) {
             for (let i = 0; i < results.length; i++) {
-              // if there is data returned, add the produce to the plots object
+              // If there is data returned, add the produce to the plots object
               if (results[i].data.produce.length > 0) {
-                // let plotId = parseInt(results[i].data.plot_id);
-                // console.log(plotId);
                 plots[i].produce = results[i].data.produce;
               } else {
                 console.error("missing produce");
@@ -51,25 +45,19 @@
             // For each plot, color the plot according to the produce entered
             for (let i = 0; i < plots.length; i++) {
               colorCluster(plots[i].produce, plots[i].svgCluster);
-              // console.log(plots[i]);
             }
-            // console.log(plots);
           });
         });
       };
 
       function colorCluster(produce, plotCluster) {
-        // if (produce[0]) {
-          plotCluster.each(function(i, el) {
-            // console.log(produce); // there should only be one value
-            let colorVar = getRandomIntInclusive(0, produce.length-1);
-            let color = '#'+produce[colorVar].color;
-            angular.element(el).attr("style",'fill:' + color);
-          });
-        // }
-
+        plotCluster.each(function(i, el) {
+          let colorVar = getRandomIntInclusive(0, produce.length-1);
+          let color = '#'+produce[colorVar].color;
+          angular.element(el).attr("style",'fill:' + color);
+        });
       }
-      // range is 0, 0 for now becuase there is only 1 entry in the produce arr
+
       function getRandomIntInclusive(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
