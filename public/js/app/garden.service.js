ace7bafb8e54f78bea7128505c549a89541f5429
Anna Lotko <annaklotko@gmail.com>
2017-02-03 10:51:24 -0700

    fixed bug where service persists plot data

--- public/js/app/garden.service.js
+++ public/js/app/garden.service.js
@@ -4,30 +4,33 @@
   angular.module('app')
     .service('gardenService', function() {
       this.gardenID = "gardenID";
-      this.gardenPlots = [];
+      this.plots = [];
 
       this.getPlots = function($http, thisGarden) {
+        var that = this;
+        that.plots.length = 0;
+
         // Get all plot data and filter by garden
         $http.get('/api/plots').then(function(result) {
-          var plots = [];
+          // var plots = [];
           for (let i = 0; i < result.data.length; i++) {
             if (result.data[i].farm === thisGarden) {
-              plots.push(result.data[i]);
+              that.plots.push(result.data[i]);
             }
           }
 
           // Get all SVG clusters for each plot and add cluster to object reference
           let itr = 0;
-          for (let i = plots[0].id; i <= plots[plots.length-1].id; i++) {
-            plots[itr].svgCluster = angular.element('#p'+i).children();
+          for (let i = that.plots[0].id; i <= that.plots[that.plots.length-1].id; i++) {
+            that.plots[itr].svgCluster = angular.element('#p'+i).children();
             itr++;
           }
 
           // Add an array of produce data to the plot and populate it
           var promiseArr = [];
-          for (let i = 0; i < plots.length; i++) {
+          for (let i = 0; i < that.plots.length; i++) {
             // i needs to start at 209
-            let plotId = plots[0].id + i;
+            let plotId = that.plots[0].id + i;
             // console.log(plotId);
             let promise = $http.get(`api/producePlots/${plotId}`);
             promiseArr.push(promise);
@@ -37,14 +40,15 @@
             for (let i = 0; i < results.length; i++) {
               // If there is data returned, add the produce to the plots object
               if (results[i].data.produce.length > 0) {
-                plots[i].produce = results[i].data.produce;
+                that.plots[i].produce = results[i].data.produce;
               } else {
                 console.error("missing produce");
               }
             }
             // For each plot, color the plot according to the produce entered
-            for (let i = 0; i < plots.length; i++) {
-              colorCluster(plots[i].produce, plots[i].svgCluster);
+            for (let i = 0; i < that.plots.length; i++) {
+              // console.log(that.plots[i].svgCluster);
+              colorCluster(that.plots[i].produce, that.plots[i].svgCluster);
             }
           });
         });
