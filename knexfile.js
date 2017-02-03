9d40a68ae9bc29349d9848a445828a7437e2bb0b
Anna Lotko <annaklotko@gmail.com>
2017-02-03 11:22:33 -0700

    updated json package

--- knexfile.js
+++ knexfile.js
@@ -9,30 +9,4 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL
   }
-
-  // development: {
-  //   client: 'mysql',
-  //   connection: {
-  //     host: 'localhost',
-  //     user: 'root',
-  //     password: '',
-  //     database: 'growing_gardens_dev'
-  //   }
-  // },
-  //
-  // production: {
-  //   client: 'mysql',
-  //   connection: 'mysql://b13910f57ab565:d0f6cd3e@us-cdbr-iron-east-04.cleardb.net/heroku_b53d242096e38d5?reconnect=true',
-  //   // migrate: 'safe',
-  //   pool: {
-  //     max: 1,
-  //     min: 0
-  //   }
-  //   // migrations: {
-  //   //   directory: './migrations'
-  //   // },
-  //   // seeds: {
-  //   //   directory: './seeds'
-  //   // }
-  // }
 };
